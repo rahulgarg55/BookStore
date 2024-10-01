3c0e9172bc6b09ebdfa9cb93ac6cab1271fc4cbf
rahulgarg55 <gargr010>
2024-10-01 12:48:28 +0530

    nodemon

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const dotenv=require("dotenv");
 dotenv.config(); 
 const bookroute=require("./routes/bookRoutes");
 
-
 mongoose.connect(process.env.MONGO_URI,{
 useNewUrlParser:true, 
 useUnifiedTopology:true,
